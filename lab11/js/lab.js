ea46aecbdfccecdcac179d3accdb760827343803
Devsub05 <Devsub05@gmail.com>
2025-05-19 16:42:02 -0700

    lab 11 changes

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,39 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * lab.js - Working with your partner, experiment with JS events.
+This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
 
-// Constants
+ * Created: May 19, 2025
 
-// Functions
+ * Requirements: jQuery must be loaded for this script to work
+ * Author: Devika Subramaniam
+*/
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sort the characters of a string in alphebetical order
+function sortString(inputString) {
+  // Convert string to array, arrange it, then join it back into the string
+  return inputString.split('').sort().join('');
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Click listener for the button
+$('#submit').click(function(){
+  // Get value from input field
+  const userName= $("#user-name").val();
 
-// let's get this party started
-main();
+  //Debug
+  console.log("User input:", userName);
+
+  // Sort the String
+  const userNameSorted = sortString(userName);
+
+  //Debug
+   console.log("Sorted name:", userNameSorted);
+
+  //Display it in #output
+  $("#output").append(`
+    <div class="text" style="display:none;">
+      <p>${userNameSorted}</p>
+    </div>
+  `);
+  $(".text:last").fadeIn(500);  
+});
+ 
