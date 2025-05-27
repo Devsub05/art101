b89c0a212bd4fa7c360b92e0df60c4091b2844ea
Devsub05 <Devsub05@gmail.com>
2025-05-26 17:18:47 -0700

    test lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,21 +1,48 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * lab 13: Loops
 
-// Constants
+* This program is intended to loop through number 1-200 and prints:
 
-// Functions
+- "Fizz!" if divisible by 3
+- "Buzz!" if divisible by 5
+- "Boom!" if divisible by 7
+* Combines them for numbers divisible by the multiple
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+
+ * Created: May 22, 2025
+ * Author: Devika Subramaniam
+*/
+
+// Wait for the document to completely load
+$(document).ready(function() {
+  // Call the function
+  fizzBuzzBoom();
+});
+
+function fizzBuzzBoom() {
+  // Loop from 1-200
+  for (let i = 1; i <= 200; i++) {
+    let str = ""; //--> Reset string for each of the numbers
+
+    // check for multiples and add the text according to that
+    if (i % 3 === 0) str += "Fizz";
+    if (i % 5 === 0) str += "Buzz";
+    if (i % 7 === 0) str += "Boom";
+
+    // if there is no match just return the number
+    if (str === "") {
+      str = i;
+    } else {
+      str = i + " - " + str + "!";
+    }
+
+    //debug
+    console.log("i:", i, "→", str);
+    
+    // Append the result above to the output div
+    $("#output").append("<p>" + str + "</p>");
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+
